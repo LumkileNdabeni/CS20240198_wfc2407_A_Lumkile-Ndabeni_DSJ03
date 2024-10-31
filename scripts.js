03f4c05de0fee31c942371162b68b6e8ce154681
Lumkile Ndabeni <ndabenilumkile@gmail.com>
2024-10-31 21:57:30 +0200

    Initialized app/functions

--- scripts.js
+++ scripts.js
@@ -1,29 +1,18 @@
 import { books, authors, genres, BOOKS_PER_PAGE } from './data.js'
 
-let page = 1;
-let matches = books
+const BookApp = {
+    page: 1,
+    matches: books,
 
-const starting = document.createDocumentFragment()
+    // Initialize the application
+    init() {
+        this.populateBookPreviews(this.matches.slice(0, BOOKS_PER_PAGE));
+        this.populateDropdowns();
+        this.setTheme();
+        this.updateShowMoreButton();
+        this.setupEventListeners();
+    },
 
-for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-    const element = document.createElement('button')
-    element.classList = 'preview'
-    element.setAttribute('data-preview', id)
-
-    element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
-        
-        <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
-    `
-
-    starting.appendChild(element)
-}
 
 document.querySelector('[data-list-items]').appendChild(starting)
 
