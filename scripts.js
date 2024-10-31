e10d74f0db43b88a80b0876bd57463d4ac83e1a3
Lumkile Ndabeni <ndabenilumkile@gmail.com>
2024-10-31 22:33:02 +0200

    Corrected function to create book preview

--- scripts.js
+++ scripts.js
@@ -13,6 +13,22 @@ const BookApp = {
         this.setupEventListeners();
     },
 
+    
+    // Function to create and return a book preview element
+    createBookPreviewElement({ author, id, image, title }) {
+        const element = document.createElement('button');
+        element.classList.add('preview');
+        element.setAttribute('data-preview', id);
+        element.innerHTML = `
+            <img class="preview__image" src="${image}" />
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${authors[author]}</div>
+            </div>
+        `;
+        return element;
+    },
+
 
     // Function to populate book previews on the page
     populateBookPreviews(bookList) {
